8dca152b58d7556e6ccc41c54154778d1e2ccf33
mdayku <marcusday3586@gmail.com>
2025-12-23 14:30:56 -0600

    chore: bump version to 0.9.8 for HuggingFace Spaces sync

--- src/haoline/__init__.py
+++ src/haoline/__init__.py
@@ -23,7 +23,7 @@ Example usage:
     print(report.to_json())
 """
 
-__version__ = "0.9.7"
+__version__ = "0.9.8"
 
 from .analyzer import MetricsEngine, ONNXGraphLoader
 from .compare_visualizations import (
